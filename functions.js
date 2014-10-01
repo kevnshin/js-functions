0c56fbde260f1b4500c778afcdf07d9647e27bff
kevnshin <kevnshin@gmail.com>
2014-10-01 07:22:09 -1000

    14th Function

--- functions.js
+++ functions.js
@@ -263,6 +263,19 @@ function minimum(num1,num2){
  * @return {number} the largest number
  */
 
+function maximum(num1,num2){
+
+  if(num1>num2){
+    return  num1;
+  }
+
+  else{
+    return num2;
+  }
+
+}
+
+
 /**
  * Returns true if `n` is even.
  * @param {number} n
