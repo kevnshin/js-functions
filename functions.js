e8d017660cb9181b55a5e56422687e57b78db545
kevnshin <kevnshin@gmail.com>
2014-10-01 06:43:10 -1000

    4th Function

--- functions.js
+++ functions.js
@@ -45,6 +45,13 @@ function decrease(num){
  * @return {number} the sum
  */
 
+function add(num1,num2){
+
+  return num1+num2;
+}
+
+
+
 /**
  * Subtracts the second number from the first.
  * @param {number} x
